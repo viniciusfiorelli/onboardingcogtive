db202ddf781698ae53f012097e8a7b9a194b0cf7
Vinicius Fiorelli <vinicius.fiorelli@telerisco.com>
2026-05-07 15:36:53 -0300

    fix lighten localhost rendering

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+if (['localhost', '127.0.0.1', '::1'].includes(window.location.hostname)) {
+  document.documentElement.classList.add('local-performance-mode');
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
